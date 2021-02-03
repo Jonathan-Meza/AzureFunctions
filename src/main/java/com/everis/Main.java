f88a5a0330a371f3b07669ebc1e561c215e56548
Jonathan Hevert Meza Osorio <hevert_93@hotmail.com>
2021-02-03 14:21:32 -0500

    se agregar un ejemplo de como usar la clase Util

--- src/main/java/com/everis/Main.java
+++
@@ -1,8 +0,0 @@
-package com.everis;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bienvenidos a este programa de consola!");
-        System.out.println("---------");
-    }
-}
