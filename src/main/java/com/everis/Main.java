8617dbf6d3ce6af530bbeb6f4fa89d7f0bf0be9b
Jonathan Hevert Meza Osorio <hevert_93@hotmail.com>
2021-02-03 17:12:08 -0500

    Agregamos la opcion para que ingrese su nombre

--- src/main/java/com/everis/Main.java
+++ src/main/java/com/everis/Main.java
@@ -1,8 +1,17 @@
 package com.everis;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String name;
+
         System.out.println("Bienvenidos a este programa de consola!");
         System.out.println("---------");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa tu nombre:");
+        name = scanner.next();
+        System.out.println("Felicitaciones " + name + " ingresastes tu nombre!");
+
     }
 }
