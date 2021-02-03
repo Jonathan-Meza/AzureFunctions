0e2ebd66ab2c8c2699c5f2d0fa999a004ad3c882
Jonathan Hevert Meza Osorio <hevert_93@hotmail.com>
2021-02-03 17:29:36 -0500

    agregar la opcon de pedir celular

--- src/main/java/com/everis/AzureFunctions.java
+++ src/main/java/com/everis/AzureFunctions.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class AzureFunctions {
     public static void main(String[] args) {
+        String phone;
         System.out.println("Bienvenidos a este programa de consola!");
         System.out.println("---------");
         Scanner sc = new Scanner(System.in);
@@ -14,5 +15,8 @@ public class AzureFunctions {
         mail = sc.nextLine();
         Util util = new Util();
         System.out.println(mail + (util.isEmail(mail)?" es ":" no es ") + "correo");
+        System.out.println("Ingresa tu numero de celular");
+        phone = sc.nextLine();
+        System.out.println("Ingresastes tu numero de celular!");
     }
 }
